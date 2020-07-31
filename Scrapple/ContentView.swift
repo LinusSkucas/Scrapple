947c2a0cba4d22074cf440959a789caba1961859
Linus S <LinusS1@users.noreply.github.com>
2020-07-30 18:17:31 -0700

    Added the basics for the popover

--- Scrapple/ContentView.swift
+++ Scrapple/ContentView.swift
@@ -10,8 +10,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        HStack {
+            Text("Hello, World!")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            Button("Hello", action: { print("Hello World") })
+        }
     }
 }
 
