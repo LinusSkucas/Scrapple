db7bb5e76c4c5b165480a20ab26c24508d32a917
Linus S <LinusS1@users.noreply.github.com>
2020-07-30 17:56:54 -0700

    Initial Template

---
+++ Scrapple/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Scrapple
+//
+//  Created by Linus Skucas on 7/30/20.
+//  Copyright © 2020 Linus Skucas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
